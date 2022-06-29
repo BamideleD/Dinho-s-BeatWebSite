f52fae4f4e100d5648a01b9d19b20cd7fc8c218e
BamideleD <bamadboss@gmail.com>
2022-06-29 16:19:25 +0100

    Creating the first Beat Card

--- src/Beat.js
+++ src/Beat.js
@@ -1,4 +1,20 @@
 import React from "react";
 
 
- 
+const Beat = () => {
+    return(
+        <div>
+            <img 
+                src="https://lastfm.freetls.fastly.net/i/u/770x0/2bf9fbd3acae7f9a5615adbd71f6c7f6.jpg" alt="Little Simz" height="100" width="100" />
+            <div>
+                <b>SWING</b>
+                <p>(Little Simz Type beat)</p>
+                <audio controls src="Swing Part 2.mp3" type = {"audio/mpeg"}> </audio>
+            </div>
+        </div>
+    )
+}
+
+
+
+export default Beat;
