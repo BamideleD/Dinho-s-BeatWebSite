71109d727bd890af83537a860247c030e8d372d1
BamideleD <bamadboss@gmail.com>
2022-06-30 09:07:33 +0100

    Creating BeatList Array

---
+++ src/Beatlist.js
@@ -0,0 +1,67 @@
+export default Beatlist = [
+    {
+        title: 'SWING',
+        type: '(Little Simz Type Beat)',
+        imgsrc: "https://lastfm.freetls.fastly.net/i/u/770x0/2bf9fbd3acae7f9a5615adbd71f6c7f6.jpg",
+        imgalt: 'Little Simz',
+        beatsrc: 'Swing Part 2.mp3'
+        
+    },
+    {
+        title: 'UNTAMED',
+        type: '(MDJ Type Beat)',
+        imgsrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5zaKXZ3MgxvAfke_vdI6x9qjzP75N8fWFbqPBPopRuhU67-wyPwo7NK5j3qvXO48svh4&usqp=CAU",
+        imgalt: 'Midas The Jagaban',
+        beatsrc: 'Swing Part 2.mp3'
+    },
+    {
+        title: 'LOVE YOU',
+        type: '(Drake Type Beat)',
+        imgsrc: "https://i1.sndcdn.com/artworks-000490319808-r5ya2s-t500x500.jpg",
+        imgalt: 'Drake',
+        beatsrc: 'Swing Part 2.mp3'
+    },
+    {
+        title: 'MARCO POLO',
+        type: '(Kendrick Lamar Type Beat)',
+        imgsrc: "https://raptv.com/wp-content/uploads/lamar.png",
+        imgalt: 'Kendrick Lamar',
+        beatsrc: 'Swing Part 2.mp3'
+    },
+    {
+        title: 'SAVE SAWTEE',
+        type: '(Dave Type Beat)',
+        imgsrc: "https://i.guim.co.uk/img/media/0511ab9ac0ee1dc4f4ad76ae4c2a393d9208c170/216_164_2649_1590/master/2649.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=058cea43f3036b8fb547a4a609cc126e",
+        imgalt: 'Dave',
+        beatsrc: 'Swing Part 2.mp3'
+    },
+    {
+        title: 'SAF',
+        type: '(Masego Type Beat)',
+        imgsrc: "https://i.scdn.co/image/ab6761610000e5eb56288d938ef220c23caf2f63",
+        imgalt: 'Masego',
+        beatsrc: 'Swing Part 2.mp3'
+    },
+    {
+        title: 'HINATA',
+        type: '(Tion Wayne Type Beat)',
+        imgsrc: "https://djmag.com/sites/default/files/article/image/Screen%20Shot%202021-09-17%20at%2012.53.36.png",
+        imgalt: 'Tion Wayne',
+        beatsrc: 'Swing Part 2.mp3'
+    },
+    {
+        title: 'DK',
+        type: '(Wizkid Type Beat)',
+        imgsrc: "https://i.scdn.co/image/ab6761610000e5eb9050b61368975fda051cdc06",
+        imgalt: 'Wizkid',
+        beatsrc: 'Swing Part 2.mp3'
+    },
+    {
+        title: 'FUCK LOVE',
+        type: '(Burnaboy Type Beat)',
+        imgsrc: "https://txpush-9575.kxcdn.com/wp-content/uploads/2021/07/Burna-Boy-smiling-1024x1024.jpeg",
+        imgalt: 'Burnaboy',
+        beatsrc: 'Swing Part 2.mp3'
+    },
+    
+]
