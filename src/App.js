774679a2f8fbd1a20aec3e6c1a38932b5129e583
BamideleD <bamadboss@gmail.com>
2022-07-04 10:29:15 +0100

    Adding the footer

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ const App = () => {
             <div className="">
                 <Beatlist />
             </div>
+            <footer className="tc"> ALL MUSIC AND SERVICES BELONG AND/OR IS RENDERED BY DINHO </footer>
             
         </div>
     )
