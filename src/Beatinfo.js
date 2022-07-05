45a379983e97de131d10df1eca32fec7aa7418ff
BamideleD <bamadboss@gmail.com>
2022-07-05 11:27:26 +0100

    Adding more beat components

--- src/Beatinfo.js
+++ src/Beatinfo.js
@@ -50,7 +50,7 @@ const Beatinfo = [
         beatsrc: 'Swing Part 2.mp3'
     },
     {
-        title: 'DK',
+        title: 'ROCKY',
         type: '(Wizkid Type Beat)',
         imgsrc: "https://i.scdn.co/image/ab6761610000e5eb9050b61368975fda051cdc06",
         imgalt: 'Wizkid',
@@ -61,6 +61,111 @@ const Beatinfo = [
         type: '(Burnaboy Type Beat)',
         imgsrc: "https://txpush-9575.kxcdn.com/wp-content/uploads/2021/07/Burna-Boy-smiling-1024x1024.jpeg",
         imgalt: 'Burnaboy',
+        beatsrc: 'Swing Part 2.mp3'
+    },
+    {
+        title: 'BEARS',
+        type: '(Drake Type Beat)',
+        imgsrc: "https://media.gq.com/photos/5e18a651c630f10008cb568d/1:1/w_983,h_983,c_limit/DrakeWatch.jpg",
+        imgalt: 'Drake',
+        beatsrc: 'Swing Part 2.mp3'
+    },
+    {
+        title: 'DANKE',
+        type: '(ASAP Rocky Type Beat)',
+        imgsrc: "https://pyxis.nymag.com/v1/imgs/f00/112/51fcbe3c50d2530a4d01a1160292ddaf62-26-asap-rocky.rsquare.w700.jpg",
+        imgalt: 'ASAP Rocky',
+        beatsrc: 'Swing Part 2.mp3'
+    },
+    {
+        title: 'DK',
+        type: '(Oxlade Type Beat)',
+        imgsrc: "https://i.scdn.co/image/ab6761610000e5ebfbd014f58142c4de93e67afe",
+        imgalt: 'Oxlade',
+        beatsrc: 'Swing Part 2.mp3'
+    },
+    {
+        title: 'NORTH',
+        type: '(Kanye West Type Beat)',
+        imgsrc: "https://img.ktt2.com/artists/kanye-300x.jpg",
+        imgalt: 'Kanye West',
+        beatsrc: 'Swing Part 2.mp3'
+    },
+    {
+        title: 'OOGWAY',
+        type: '(Arrdee Type Beat)',
+        imgsrc: "http://www.femalefirst.co.uk/image-library/partners/bang/square/500/a/arrdee-hits-out-at-artists-for-treating-him-like-a-child-aW1.jpg",
+        imgalt: 'Arrdee',
+        beatsrc: 'Swing Part 2.mp3'
+    },
+    {
+        title: 'REPETE',
+        type: '(Burnaboy Type Beat)',
+        imgsrc: "https://www.informationng.com/wp-content/uploads/2021/05/grammy-award-i-had-to-choose-between-greatness-death-singer-burna-boy.jpg",
+        imgalt: 'Burnaboy',
+        beatsrc: 'Swing Part 2.mp3'
+    },
+    {
+        title: 'GUITAR SESH',
+        type: '(Digga D Type Beat)',
+        imgsrc: "https://is5-ssl.mzstatic.com/image/thumb/Features124/v4/13/d2/47/13d24738-56e6-d584-95a1-4b37b988e706/pr_source.png/300x300cc.jpg",
+        imgalt: 'Digga D',
+        beatsrc: 'Swing Part 2.mp3'
+    },
+    {
+        title: 'TTD',
+        type: '(Obongjayar Type Beat)',
+        imgsrc: "https://pbs.twimg.com/media/FG9xLFiWUAMyCQN?format=jpg&name=large",
+        imgalt: 'Obongjayar',
+        beatsrc: 'Swing Part 2.mp3'
+    },
+    {
+        title: 'MALEYA',
+        type: '(Amaarae Type Beat)',
+        imgsrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQM7F-FbCEucqDnRFwcqdW5AuNSkW8Rx2JaO2T4GcqhYgVnCUV4SDwx2MkoHVcN3v5liqQ&usqp=CAU",
+        imgalt: 'Amaarae',
+        beatsrc: 'Swing Part 2.mp3'
+    },
+    {
+        title: 'KOTO',
+        type: '(Roddy Rich Type Beat)',
+        imgsrc: "https://lastfm.freetls.fastly.net/i/u/770x0/f7414e86281cbfbd740aad31e944a54f.jpg",
+        imgalt: 'Roddy Rich',
+        beatsrc: 'Swing Part 2.mp3'
+    },
+    {
+        title: 'PERPETUATED',
+        type: '(Amaarae Type Beat)',
+        imgsrc: "https://i.scdn.co/image/b4eb5c8020332149e5fe5487e5f0d0eeb6ed5252",
+        imgalt: 'Amaarae',
+        beatsrc: 'Swing Part 2.mp3'
+    },
+    {
+        title: 'SERVE IT',
+        type: '(Jhus Type Beat)',
+        imgsrc: "https://i.iheart.com/v3/catalog/artist/31018332?ops=fit(720%2C720)",
+        imgalt: 'Jhus',
+        beatsrc: 'Swing Part 2.mp3'
+    },
+    {
+        title: 'RASTA FEAR',
+        type: '(Tems Type Beat)',
+        imgsrc: "https://data.mothership.tools/mothershiptools-ukprod/wp-content/uploads/2021/11/Tems-Square.jpg",
+        imgalt: 'Tems',
+        beatsrc: 'Swing Part 2.mp3'
+    },
+    {
+        title: 'DIOR',
+        type: '(Travis Scott Type Beat)',
+        imgsrc: "https://pyxis.nymag.com/v1/imgs/3f1/95d/83a834934bca0b873613575d3bd388f88a-06-travis-scott-1.rsquare.w700.jpg",
+        imgalt: 'Travis Scott',
+        beatsrc: 'Swing Part 2.mp3'
+    },
+    {
+        title: 'SYMPHONY',
+        type: '(Teswess Scott Type Beat)',
+        imgsrc: "https://i.scdn.co/image/ab6761610000e5eb2cc918b6f95839d5f4eacd98",
+        imgalt: 'Teswess',
         beatsrc: 'Swing Part 2.mp3'
     },
     
